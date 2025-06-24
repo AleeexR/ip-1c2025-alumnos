d09330454a8271c8b89c7044da8b617946e96b69
Alex Ramos <alexramos2086@gmail.com>
2025-06-24 19:43:00 -0300

    Modifiqué el módulo views.py dentro de la funcion "home"

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ def index_page(request):
 
 # esta función obtiene 2 listados: uno de las imágenes de la API y otro de favoritos, ambos en formato Card, y los dibuja en el template 'home.html'.
 def home(request):
-    images = []
+    images = services.getAllImages() # En esta línea de código traemos una lista de cards de Pokemons desde el módulo services.py para luego mostrarlas en pantalla.
     favourite_list = []
 
     return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
